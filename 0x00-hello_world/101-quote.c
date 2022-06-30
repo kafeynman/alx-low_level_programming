ce4e92ef4744c4bd55a6e109977c95f72af29a58
kafeynman <ishek.dev@gmail.com>
2022-06-29 22:13:39 -0700

    #101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point of program
+ * @void: takes no arguments
+ *
+ * Description: prints to o/p using write from unistd
+ * Return: 0 if successful. 1 otherwise
+ */
+int main(void)
+{
+	write(2, " and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+
+}

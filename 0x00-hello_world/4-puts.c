54f47ec24f93bc9837c26d52aa8ca3ff7968c679
kafeynman <ishek.dev@gmail.com>
2022-06-29 21:17:14 -0700

    #4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - the entry point of c programs
+ * @void: takes no arguments
+ *
+ * Description: Starting point of c program
+ * Return: 0 if successful. 1 otherwise
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+
+}

040d659770a35e2e09c8add011f5e448a4629259
kafeynman <ishek.dev@gmail.com>
2022-06-29 21:33:42 -0700

    #5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print some text
+ * @void: takes no arguments
+ *
+ * Description: Prints the phrase passed as a parameter to printf
+ * Return: 0 if successful. 1 otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}

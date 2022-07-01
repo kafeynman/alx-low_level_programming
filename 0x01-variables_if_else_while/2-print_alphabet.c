7a1efd1350c31b04928e510d8e344f83712cf753
kafeynman <ishek.dev@gmail.com>
2022-07-01 14:59:38 -0700

    printer

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ * Description: Prints the alphabet in lowercase
+ * Return: 0 successful, 1 otherwise
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	printf("%s\n", alphabet);
+
+	return (0);
+}

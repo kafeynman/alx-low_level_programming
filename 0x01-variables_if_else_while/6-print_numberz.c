bc388006781ac9f6e4d23324346679a969fe805b
kafeynman <ishek.dev@gmail.com>
2022-07-01 15:57:49 -0700

    #6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ * Description: Prints base 10 numbers
+ * Return: 0 (successful), 1 (otherwise)
+ */
+int main(void)
+{
+
+	for (int i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+
+}

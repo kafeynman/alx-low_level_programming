9497a9904706425a492bb06f1c4b32cd25936607
kafeynman <ishek.dev@gmail.com>
2022-07-05 06:20:12 -0700

    0x02

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print a string
+ * Descriptin: Print the string _putchar
+ * Return: 0 (always), 1 (otherwise)
+ */
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}

51e5dd4ea92e222f8de2fe720be5264d180e2a03
kafeynman <ishek.dev@gmail.com>
2022-07-06 16:13:17 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers in order to 98
+ * @n: the fisrt number to be printed
+ *
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i);
+			}
+		}
+	}
+	else
+	{
+		for (i = n; n <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i);
+			}
+		}
+	}
+}

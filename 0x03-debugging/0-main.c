f33cc513889f1d8fb27e523f658c55f7e2c8ddd8
kafeynman <dev.ishek@gmail.com>
2022-07-06 22:51:43 +0200

    #0 v0.1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
-* positive_or_negative - checks if a number is +ve or -ve
-* @n: an integer argument with the number to be checked
-*
-* Description: takes an integer and check if it is below 0 for +ve/-ve
-* Return: 1 (positive), 0 (zero), -1 (negative)
+* main - entry point of program
+* Description: the positve_or_negative function checks if a number is +ve/-ve
+* Return: 0
 */
 int main(void)
 {
-	int n;
-	n = 0;
+	int i;
 
-	positive_or_negative(n);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }

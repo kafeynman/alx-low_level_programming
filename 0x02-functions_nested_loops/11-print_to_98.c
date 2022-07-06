882b7a007458d77eeaf205e7c8e0c3cad18b8107
kafeynman <ishek.dev@gmail.com>
2022-07-06 16:24:55 -0700

    98 v0.2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n > 98)
+	if (n >= 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
@@ -26,7 +26,7 @@ void print_to_98(int n)
 			}
 		}
 	}
-	else
+	else if (n <= 98)
 	{
 		for (i = n; n <= 98; i++)
 		{

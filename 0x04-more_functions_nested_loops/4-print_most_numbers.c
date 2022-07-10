0349dafc8ea5096ce3460d39e96267e963f0c80b
kafeynman <ishek.dev@gmail.com>
2022-07-10 21:24:40 +0000

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,11 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 10; i++)
 	{
-		if (i != 52 || i != 50)
+		if (i != 2 && i != 4)
 		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
 
 		_putchar('\n');

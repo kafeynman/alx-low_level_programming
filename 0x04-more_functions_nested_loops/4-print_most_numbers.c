a82731326765ae821f9ea507bf2d4ea0c9c39835
kafeynman <ishek.dev@gmail.com>
2022-07-10 20:57:09 +0000

    add numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints number
+* Description: print numbers except 2 and 4
+* Return: nothing
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 52 || i == 50)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+
+		_putchar('\n');
+	}
+}

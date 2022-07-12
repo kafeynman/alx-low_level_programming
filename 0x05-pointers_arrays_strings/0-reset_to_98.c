f8d79ba211afe930eab6f3785f130603da8c6553
kafeynman <ishek.dev@gmail.com>
2022-07-12 07:21:10 +0000

    initialising

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* reset_to_98 - always resets the parameter to 98.
+* @n: a pointer to an int.
+*
+* Description: the functions takes in an argument and prints the value
+*		after printing the value it resets the value of the argument
+*		to 98.
+* Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	n = 98;
+	printf("n=%d\n", n);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains pexercises on Pointers, Arrays and Strings

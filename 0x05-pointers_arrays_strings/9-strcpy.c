3287bf2d681a7aeaef977bdaf9ba7c3a25578c04
Kafeynman <105448459+kafeynman@users.noreply.github.com>
2022-07-12 15:42:16 +0200

    Update 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* rev_string - reverses a string.
+* @s: string arg.
+* Return: nothing.
+*/
+
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char *s_cp;
+
+	while (s[i] != '\0')
+	{
+		len++;
+	}
+	s_cp = (char *)malloc(len * sizeof(char));
+	i = 0;
+	--len;
+
+	if (s_cp == NULL)
+	{
+		exit(0);
+	}
+	else
+	{
+		while (s[i] != '\0')
+		{
+			s_cp[i] = s[len];
+			i++;
+			len--;
+		}
+		i = 0;
+	}
+	while (s_cp[i] != '\0')
+	{
+		s[i] = s_cp[i];
+		i++;
+	}
+	free(s_cp);
+}

7b7f079f63900597526a435f3072a744e190a3a4
kafeynman <ishek.dev@gmail.com>
2022-07-12 14:02:15 +0000

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,28 +10,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int len = 0, i = 0;
+	int i = 0;
 
 	while (src[i] != '\0')
 	{
-		len++;
+		dest[i] = src[i];
 		i++;
 	}
-
-	dest = (char *)malloc(len * sizeof(char));
-
-	if (dest == NULL)
-	{
-		exit(0);
-	}
-	else
-	{
-		while (src[i] != '\0')
-		{
-			dest[i] = src[i];
-			i++;
-		}
-	}
+	dest[i] = '\0';
 
 	return (dest);
 }

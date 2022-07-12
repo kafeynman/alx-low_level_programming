4e19e34fbc999b030d202c00ff7654a4eef83a9a
kafeynman <ishek.dev@gmail.com>
2022-07-12 22:43:55 +0000

    atoi v0.4

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,9 +18,9 @@ int _atoi(char *s)
 		}
 		else if (s[i] == '-')
 		{
-			res * (-1);
+			res *= -1;
 		}
-		else if (_isdigit(s[i]) == 0 && _isdigit(s[i -1]) == 1)
+		else if (_isdigit(s[i]) == 0 && _isdigit(s[i - 1]) == 1)
 		{
 			break;
 		}
@@ -29,9 +29,15 @@ int _atoi(char *s)
 	return (res);
 }
 
+/**
+* _isdigit - checks if an alpha is a digit
+* @c: argument
+* Return: 1 if digit, 0 otherwise
+*/
+
 int _isdigit(int c)
 {
-	if ( c >= '0' && c <= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);

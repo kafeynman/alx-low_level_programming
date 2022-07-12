1ace408d31faa8f5b32a84251d3950c8d71a5d34
kafeynman <ishek.dev@gmail.com>
2022-07-12 13:34:49 +0000

    k

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,34 @@
 void rev_string(char *s)
 {
 	int len = 0, i = 0;
-	/* char *s_cp;*/
+	char *s_cp;
 
 	while (s[i] != '\0')
 	{
 		len++;
 		i++;
 	}
-	_putchar((len % 10) + '0');
+	/* allocate mem to s_cp of lenght len */
+	s_cp = (char *)malloc(len * sizeof(char));
+	i = 0;
+	--len;
+	if (s_cp == NULL)
+	{
+		exit(0);
+	}
+	else
+	{
+		while (s[i] != '\0')
+		{
+			s_cp[i] = s[len];
+			i++;
+			len--;
+		}
+		i = 0;
+	}
+	while (s[i] != '\0')
+	{
+		s[i] = s_cp[i];
+		i++;
+	}
 }

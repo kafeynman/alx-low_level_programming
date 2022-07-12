65574d9f33960461273595e840c869ab7df2a187
kafeynman <ishek.dev@gmail.com>
2022-07-12 22:40:00 +0000

    atoi v0.3

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,11 +12,27 @@ int _atoi(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= '0' && s[i] <= '9')
+		if (_isdigit(s[i]) == 1)
 		{
 			res = res * 10 + s[i] - '0';
+		}
+		else if (s[i] == '-')
+		{
+			res * (-1);
+		}
+		else if (_isdigit(s[i]) == 0 && _isdigit(s[i -1]) == 1)
+		{
+			break;
 		}
 	}
 
 	return (res);
+}
+
+int _isdigit(int c)
+{
+	if ( c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
